fb96576e01ec3aa0c6ed905d71a21125596974e3
Naomi Dennis <njdennis94@gmail.com>
2018-11-01 16:34:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+  // describe('createPost', () => {
+  //     it('exists', () => {
+  //       expect(createPost).toExist("Must have a function named createPost")
+  //     })
+  //     it('builds the proper templates', () => {
+  //       var spy = expect.spyOn(window._, 'template').andCallThrough()
+  //       createPost()
+  //       expect(spy).toHaveBeenCalledWith(document.getElementById("page-template").innerHTML)
+  //       expect(spy).toHaveBeenCalledWith(document.getElementById("post-template").innerHTML)
+  //     })
+  //   })
+function createPost(){
+   console.log("Test")
+  let template_data = "<div id='page-template'></div>"
+  let template_foo = _.template(template_data)
+  
+  document.getElementById("post-form").innerHTML += template_foo()
+  
+}
